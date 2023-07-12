440a442072837fe04e2246a78e6257734b4b7183
Hassan Fathy <hassanfathy460@gmail.com>
2023-07-12 21:33:58 +0300

    create portals

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from "react";
+import ReactDom from "react-dom";
 import Styles from "./Modal.module.css";
 
 const Modal = () => {
     return (
         <Fragment>
-            <div className={Styles.backdrop}></div>
-            <div className={Styles.overlay}></div>
+            {ReactDom.createPortal(
+                <Fragment>
+                <div className={Styles.backdrop}></div>
+                <div className={Styles.overlay}></div>
+                </Fragment>,
+                document.getElementById("modal")
+            )}
         </Fragment>
     );
 };
