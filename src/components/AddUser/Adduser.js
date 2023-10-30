3523e65c3f2b79990412bb0dcb307328ad11c14e
Hassan Fathy <hassanfathy460@gmail.com>
2023-10-30 20:55:42 +0200

    Update Adduser.js

--- src/components/AddUser/Adduser.js
+++ src/components/AddUser/Adduser.js
@@ -28,33 +28,33 @@ const AddUser = ({ addNewUser, closeModal }) => {
         setAddress('');
         closeModal();
     };
-    return ( 
+    return (
         <Form onSubmit={onSubmitHandler}>
-        <Form.Controller>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder="Enter Your Name" value={name} onChange={(e) => setName(e.target.value)}></input>
-        </Form.Controller>
-        <Form.Controller>
-            <label htmlFor="age">Age</label>
+            <Form.Controller>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" placeholder="Enter Your Name" value={name} onChange={(e) => setName(e.target.value)}></input>
+            </Form.Controller>
+            <Form.Controller>
+                <label htmlFor="age">Age</label>
                 <input type="text" id="age" placeholder="Enter Your Age" value={age} onChange={(e) => setAge(e.target.value)}></input>
-        </Form.Controller>
-        <Form.Controller>
-            <label htmlFor="phone">Phone</label>
-            <input type="text" id="phone" placeholder="Enter Your Phone" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
-        </Form.Controller>
-        <Form.Controller>
-            <label htmlFor="address">Address</label>
-            <input type="text" id="address" placeholder="Enter Your Address" value={address} onChange={(e) => setAddress(e.target.value)}></input>
-        </Form.Controller>
-        <Form.Controller>
-            <label htmlFor="type">Type</label>
-            <input type="text" id="type" placeholder="Enter Your Type" value={type} onChange={(e) => setType(e.target.value)}></input>
-        </Form.Controller>
-        <Row>
-            <Button type="submit">Save</Button>
-            <Button type="reset">Reset</Button>
-        </Row>
-    </Form>
+            </Form.Controller>
+            <Form.Controller>
+                <label htmlFor="phone">Phone</label>
+                <input type="text" id="phone" placeholder="Enter Your Phone" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
+            </Form.Controller>
+            <Form.Controller>
+                <label htmlFor="address">Address</label>
+                <input type="text" id="address" placeholder="Enter Your Address" value={address} onChange={(e) => setAddress(e.target.value)}></input>
+            </Form.Controller>
+            <Form.Controller>
+                <label htmlFor="type">Type</label>
+                <input type="text" id="type" placeholder="Enter Your Type" value={type} onChange={(e) => setType(e.target.value)}></input>
+            </Form.Controller>
+            <Row>
+                <Button type="submit">Save</Button>
+                <Button type="reset">Reset</Button>
+            </Row>
+        </Form>
     );
 }
 
